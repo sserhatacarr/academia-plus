a7cc1f6bd44fc7d6accfb61b7c901031e2838a1d
Serhat Acar <sserhatacarr@gmail.com>
2024-03-29 18:39:19 +0300

    first commit

---
+++ src/main/java/dev/serhatacar/academiaplus/AcademiaPlusApplication.java
@@ -0,0 +1,13 @@
+package dev.serhatacar.academiaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/serhatacar/academiaplus/AcademiaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.serhatacar.academiaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
